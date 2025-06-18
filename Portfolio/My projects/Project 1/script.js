0bb02cfab56d9349434a468ff83a00d664dbcf2a
harsh722004 <harsh7220047@gmail.com>
2025-06-18 09:33:22 +0530

    3

--- Portfolio/My projects/Project 1/script.js
+++ Portfolio/My projects/Project 1/script.js
@@ -1,47 +1,83 @@
-const root = document.getElementById('root');
+let users = [];
+let user = {};
 
-const loginform = () => {
-  const str = `
-    <h3>Login form</h3>
-     <label for="name">Name:</label>
-      <input type="text" id="name" placeholder="Enter your name" required />
-      <br /><br />
-      <label for="password">Password:</label>
-      <input type="password" id="password" placeholder="Enter your password" required />
-      <br /><br />
+const validateUser = () => {
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+  const found = users.find(
+    (value) => value.email === email && value.pass === pass
+  );
+  if (found) {
+    showHome();
+  } else {
+    document.getElementById("errorTxt").innerHTML = "Access Denied";
+  }
+};
+
+const renderUserList = () => {
+    let x= "<h4>Registered Users:</h4>";
+    if(users.length==0){
+        return "<p>No Users Registered</p>";
+
+    }
+    users.forEach(user => {
+        x += `<p>${user.name} | ${user.email} | ${user.pass} | ${user.balance}</p>`;
+    });
     
-    <p><button onclick="showhome()">Home</button></p>
-    <p><button onclick="registerform()">Create account</button></p>
-  `;
-  root.innerHTML = str;
+    return x;
 };
 
-const registerform = () => {
+const loginForm = () => {
   const str = `
-    <h3>Register form</h3>
-     <label for="name">Name:</label>
-      <input type="text" id="name" placeholder="Enter your name" required />
-      <br /><br />
-      <label for="password">Password:</label>
-      <input type="password" id="password" placeholder="Enter your password" required />
-        <br /><br />
-      <label for="password">Confirm Password:</label>
-      <input type="password" id="password" placeholder="confirm your password" required />
-      
-      <br /><br />
-    <p><button onclick="loginform()">Login</button></p>
-
-  `;
+    <div style='display:flex'>
+    <div>
+    <h3>Login Form</h3>
+    <p id='errorTxt'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='validateUser()'>Submit</button></p>
+    <p><button onclick='registerForm()'>Create Account</button></p>
+    </div>
+    <div>
+     ${renderUserList()}
+    </div>
+    </div>
+    `;
   root.innerHTML = str;
 };
 
-const showhome = () => {
-  const str = `
-    <h3>Home page</h3>
-
-    <p><button onclick="loginform()">Login</button></p>
-    <p><button onclick="registerform()">Register</button></p>
-  `;
-  root.innerHTML = str;
+const saveUser = () => {
+  let name = document.getElementById("txtName").value;
+  let email = document.getElementById("txtEmail").value;
+  let pass = document.getElementById("txtPass").value;
+  users.push({
+    name,
+    email,
+    pass,
+    balance:1000,
+  });
+  loginForm();
 };
 
+const registerForm = () => {
+  const str = `<div>
+    <h3>Registration Form</h3>
+    <p><input type='text' id='txtName'></p>
+    <p><input type='text' id='txtEmail'></p>
+    <p><input type='password' id='txtPass'></p>
+    <p><button onclick='saveUser()'>Submit</button></p>
+    <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
+    `;
+  root.innerHTML = str + "</div>";
+};
+
+const showHome = () => {
+  const str = `<div>
+    <h3>Welcome</h3>
+    <p><button onclick='loginForm()'>Logout</button></p>
+    `;
+  root.innerHTML = str + "</div>";
+};
+
+
+loginForm()
